4506a7ca9b14b5f89ee36d354aaea0fc3d667f47
aabbasali_ntst <aabbasali@ntst.com>
2022-12-26 13:48:22 +0530

    Init Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el);
+
+function App() {
+  let message = "Hi there, how are you?"
+  return (<h1>{message}</h1>);
+}
+
+root.render(<App />);
